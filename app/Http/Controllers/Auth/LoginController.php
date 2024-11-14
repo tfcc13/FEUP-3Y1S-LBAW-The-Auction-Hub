14b6c29a88f9625faca3ad54ebdb6cb5c23ff34a
avareno <miguelcabral798@gmail.com>
2024-11-14 16:35:38 +0000

    Deleted old file, updated controllers to redirect allways to the login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         if (Auth::check()) {
-            return redirect('/cards');
+            return redirect('/login');
         } else {
             return view('auth.login');
         }
@@ -38,7 +38,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials, $request->filled('remember'))) {
             $request->session()->regenerate();
  
-            return redirect()->intended('/cards');
+            return redirect()->intended('/login');
         }
  
         return back()->withErrors([
@@ -54,7 +54,6 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('login')
-            ->withSuccess('You have logged out successfully!');
+        return redirect()->route('login')->withSuccess('You have logged out successfully!');
     } 
 }
