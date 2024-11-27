1a0ef0334929b3052834979d77c1335c620303e8
avareno <miguelcabral798@gmail.com>
2024-11-27 21:21:44 +0000

    additional styling

--- resources/views/pages/user/profile.blade.php
+++ resources/views/pages/user/profile.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class="p-4">
-  <div class="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-8 gap-6">
+  <div class="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-8 gap-6 md:h-full">
 
     <!-- Left Side: Picture and Description -->
-    <div class="md:col-start-2 lg:col-start-2 md:col-span-1 lg:col-span-2">
+    <div class="md:col-start-2 border border-gray-700 rounded shadow  lg:col-start-2 md:col-span-1 lg:col-span-2 md:h-full">
 
       <!-- Picture Section -->
-      <div class="w-full p-4 border border-gray-200 rounded shadow md:p-6 dark:border-gray-700">
+      <div class="w-full p-4 md:p-6">
         <!-- User Profile Picture -->
         <div class="flex items-center justify-center h-48 mb-4 rounded overflow-hidden">
           <div class="w-48 h-48">
@@ -16,6 +16,8 @@
           </div>
         </div>
 
+      </div>
+      <div class="lg:m-5">
         <!-- User Name -->
         <div class="text-xl font-bold text-gray-900 mb-2">
           <span class="mr-2">Name:</span>
@@ -48,23 +50,23 @@
     <div class="md:col-span-3 lg:col-span-4 border dark:border-gray-700">
       <!-- Auctions Zone -->
       <div class="mt-6">
-        <!-- User's Auctions -->
-        <div class="bg-white shadow-lg rounded-lg p-6">
+        <!-- User's Auctions - Slide Component -->
+        <div class="bg-white shadow-lg rounded-lg p-2">
           <h4 class="text-lg font-semibold mb-4">Your Auctions</h4>
-          <ul>
-            @forelse (auth()->user()->ownAuction as $auction)
-            <li class="mb-2 text-gray-700">
-              <a href="{{ route('auctions.show', $auction->id) }}" class="text-blue-600 hover:underline">
-                {{ $auction->title }}
-              </a>
-              <span class="ml-2 text-sm text-gray-500">
-                ({{ $auction->state }})
-              </span>
-            </li>
-            @empty
-            <li class="text-gray-500">You don't own any auctions yet.</li>
-            @endforelse
-          </ul>
+
+
+          <x-slide.slide width="100%" height="470px">
+            @foreach (auth()->user()->ownAuction as $auction)
+            <x-slide.slide-item
+              :title="$auction->title"
+              :currentBid="$auction->current_bid"
+              :imageUrl="asset('' . $auction->primaryImage())"
+              :buttonAction="route('auctions.show', $auction->id)"
+              size="w-[300px]"
+              height="h-[200px]" />
+            @endforeach
+          </x-slide.slide>
+
         </div>
       </div>
     </div>
