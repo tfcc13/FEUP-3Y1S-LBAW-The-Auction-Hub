84348d8c6ae42a705494fd164864442f8d81c653
Tiago <tiagofccarvalho13@gmail.com>
2024-11-27 23:20:08 +0000

    experimented images, failed

--- resources/views/pages/create_auction.blade.php
+++ resources/views/pages/create_auction.blade.php
@@ -1,19 +1,5 @@
 @extends('layouts.app')
 @section('content')
-@if(session('success'))
-    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
-        {{ session('success') }}
-    </div>
-@endif
-
-@if($errors->any())
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
-        @foreach ($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
-    </div>
-@endif
-
     <div class="max-w-2xl mx-auto bg-white shadow-md rounded p-6 mt-6">
         <h2 class="text-xl font-semibold text-gray-800">Create a New Auction</h2>
 
@@ -29,7 +15,7 @@
             </div>
         @endif
 
-        <form class="flex flex-col" method="POST" action="{{ route('auctions.submit_auction') }}">
+        <form class="flex flex-col" method="POST" action="{{ route('auctions.submit_auction') }}" enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
                 <label for="title" class="block text-gray-700 font-semibold mb-2">Auction Title</label>
