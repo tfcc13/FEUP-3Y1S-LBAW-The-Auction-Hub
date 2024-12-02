b30c374a4e40a1e8d2683762f470e51c11ca3311
Afonso Pereira <afonsogpereira2003@gmail.com>
2024-12-02 19:19:14 +0000

    nav bar styling

--- resources/views/components/categories/categories.blade.php
+++ resources/views/components/categories/categories.blade.php
@@ -13,50 +13,41 @@
         'Electronics' => 'devices',
     ];
 
-    $mappedCategories = $categories->filter(function($category) use ($iconMap) {
+    $mappedCategories = $categories->filter(function ($category) use ($iconMap) {
         return isset($iconMap[$category->name]);
     });
 
-    $unmappedCategories = $categories->filter(function($category) use ($iconMap) {
+    $unmappedCategories = $categories->filter(function ($category) use ($iconMap) {
         return !isset($iconMap[$category->name]);
     });
-    
+
     $hasUnmappedCategories = $unmappedCategories->isNotEmpty();
 @endphp
 
 <nav aria-label="Product Categories" class="w-full px-52">
     <div class="flex items-center justify-between w-full" role="list">
         @foreach ($mappedCategories as $category)
-            <x-categories.category-item
-                :text="$category->name"
-                :icon="$iconMap[$category->name]"
-                {{-- :onclick="$searchCategory($text)"  --}}
-            />
+            <x-categories.category-item :text="$category->name" :icon="$iconMap[$category->name]" {{-- :onclick="$searchCategory($text)"  --}} />
         @endforeach
 
         @if ($hasUnmappedCategories)
             <x-popup.popup position="bottom" :offset="-27">
                 <x-slot:trigger>
-                    <x-categories.category-item
-                        text="More Categories"
-                        icon="more_horiz"
-                        onClick="togglePopup(this)" />
+                    <x-categories.category-item text="More Categories" icon="more_horiz" onClick="togglePopup(this)" />
                 </x-slot>
-                
+
                 <x-slot:content>
                     <div class="space-y-4" role="list">
                         @foreach ($unmappedCategories as $category)
-                            <div 
-                                class="flex items-center gap-3 p-2 hover:bg-gray-100 rounded-md cursor-pointer" 
-                                role="listitem"
-                                tabindex="0">
+                            <div class="flex items-center gap-3 p-2 hover:bg-gray-100 rounded-md cursor-pointer"
+                                role="listitem" tabindex="0">
                                 <span class="material-symbols-outlined" aria-hidden="true">category</span>
                                 <span>{{ $category->name }}</span>
                             </div>
                         @endforeach
                     </div>
                 </x-slot>
-            </x-popup>
+                </x-popup>
         @endif
     </div>
 </nav>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,81 +1,118 @@
-<nav class="bg-[#135d3b]">
-    <div class="w-full flex flex-wrap items-center justify-between p-6">
-        <a href="{{ url('/home') }}" class="flex items-center space-x-2">
-            <img src="{{ asset('images/crown_logo.webp') }}" alt="The Auction Hub Logo" class="w-10 object-contain">
-            <span class="text-xl font-semibold text-white">The Auction Hub</span>
-        </a>
+<nav class="bg-[#135d3b] w-full flex items-center justify-between p-6">
 
-        <button data-collapse-toggle="navbar-default" type="button"
-            class="bg-[#0ff290] inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-        </button>
+    <a href="{{ url('/home') }}" class="flex items-center space-x-2">
+        <img src="{{ asset('images/crown_logo.webp') }}" alt="The Auction Hub Logo" class="w-10 object-contain">
+        <span class="hidden sm:block text-xl font-semibold text-white text-nowrap">The Auction Hub</span>
+    </a>
 
-        <div class="sd:w-full md:w-1/4 hidden md:flex">
-            <form action="{{ url('/auctions/search') }}" method="GET" class="relative w-full">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg class="w-4 h-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                </span>
-                <input id="search-navbar" type="text" name="search" placeholder="Search..."
-                    class="form-input text-sm w-[25vw] !pl-10">
-            </form>
-        </div>
-
-        <div class="hidden w-full md:flex md:w-auto" id="navbar-default">
-            <ul class="flex flex-col md:flex-row md:space-x-4">
-                @if (Auth::check())
-                    <li class="flex items-center justify-center">
-                        <a href="#"
-                            class="px-2 py-[0.375rem] font-semibold text-white bg-[#135d3b] hover:bg-[#0f4a2f] rounded-lg w-28 text-center transition-transform hover:scale-105 active:scale-95">
-                            My Auctions
-                        </a>
-                    </li>
-                    <li class="flex items-center justify-center">
-                        <a href="#"
-                            class="px-2 py-[0.375rem] font-semibold text-white bg-[#135d3b] hover:bg-[#0f4a2f] rounded-lg w-28 text-center transition-transform hover:scale-105 active:scale-95">
-                            Following
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ url('/dashboard') }}"><x-user.image></x-user></a>
-                    </li>
-                @else
-                    <li class="flex items-center justify-center">
-                        <a href="{{ url('/register') }}"
-                            class="px-2 py-[0.375rem] font-semibold text-[#135d3b] bg-white hover:bg-gray-100 rounded-lg w-28 text-center transition-transform hover:scale-105 active:scale-95">
-                            Register
-                        </a>
-                    </li>
-                    <li class="flex items-center justify-center">
-                        <a href="{{ url('/login') }}"
-                            class="px-2 py-[0.375rem] font-semibold text-white bg-[#135d3b] hover:bg-[#0f4a2f] rounded-lg w-28 text-center transition-transform hover:scale-105 active:scale-95">
-                            Login
-                        </a>
-                    </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-
-    <div class="sd:w-full md:w-1/4 lg:w-1/4 mx-2 md:hidden">
-        <form action="{{ url('/auctions/search') }}" method="GET" class="relative">
+    <div class="flex w-1/2 md:w-1/4">
+        <form action="{{ url('/auctions/search') }}" method="GET" class="relative w-full">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                 <svg class="w-4 h-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
             </span>
-            <input id="search-navbar" type="text" name="search" placeholder="Search..." class="form-input pl-10">
+            <input id="search-navbar" type="text" name="search" placeholder="Search..."
+                class="form-input text-sm w-full !pl-10">
         </form>
     </div>
+
+
+    <ul class="flex w-auto space-x-4" id="navbar-default">
+        @if (Auth::check())
+            <li class="hidden md:flex items-center justify-center">
+                <a href="#"
+                    class="px-2 py-[0.375rem] font-semibold text-[#135d3b] bg-white hover:bg-gray-300 rounded-lg w-32 
+                            text-center transition-all active:scale-95">
+                    My Auctions
+                </a>
+            </li>
+            <li class="hidden md:flex items-center justify-center">
+                <a href="#"
+                    class="px-2 py-[0.375rem] font-semibold text-white bg-[#135d3b] hover:bg-[#0f4a2f] rounded-lg w-32 
+                            text-center transition-all active:scale-95">
+                    Following
+                </a>
+            </li>
+            <li class="flex items-center justify-center list-none">
+                <x-popup.popup position="bottom-left" width="w-44">
+                    <x-slot:trigger>
+                        <button onclick="togglePopup(this)"
+                            class="w-10 h-10 rounded-full overflow-hidden transition-all duration-150 active:scale-95 
+                                    hover:shadow-lg focus:outline-none">
+                            <x-user.image class="w-full h-full object-cover" />
+                        </button>
+                    </x-slot:trigger>
+
+                    <x-slot:content>
+                        <div class="space-y-2 md:-mt-2">
+                            {{-- Mobile Navigation --}}
+
+                            <a href="#"
+                                class="md:hidden flex items-center space-x-2 py-2 px-4 text-gray-800 hover:bg-[#135d3b]/15 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                </svg>
+                                <span>My Auctions</span>
+                            </a>
+                            <a href="#"
+                                class="md:hidden flex items-center space-x-2 py-2 px-4 text-gray-800 hover:bg-[#135d3b]/15 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                                </svg>
+                                <span>Following</span>
+                            </a>
+
+                            <a href="{{ url('/dashboard') }}"
+                                class="flex items-center space-x-2 py-2 px-4 text-gray-800 hover:bg-[#135d3b]/15 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                                <span>Profile</span>
+                            </a>
+
+                            <a href="{{ url('logout') }}"
+                                class="flex items-center space-x-2 py-2 px-4 text-gray-800 hover:bg-[#135d3b]/15 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                                    <polyline points="16 17 21 12 16 7"></polyline>
+                                    <line x1="21" y1="12" x2="9" y2="12">
+                                    </line>
+                                </svg>
+                                <span>Logout</span>
+                            </a>
+                        </div>
+                    </x-slot:content>
+                </x-popup.popup>
+            </li>
+        @else
+            <li class="hidden md:flex items-center justify-center">
+                <a href="{{ url('/register') }}"
+                    class="px-2 py-[0.375rem] font-semibold text-[#135d3b] bg-white hover:bg-gray-300 rounded-lg w-32 
+                            text-center transition-all active:scale-95">
+                    Register
+                </a>
+            </li>
+            <li class="flex items-center justify-center">
+                <a href="{{ url('/login') }}"
+                    class="px-2 py-[0.375rem] font-semibold text-white bg-[#135d3b] hover:bg-[#0f4a2f] rounded-lg w-auto sm:w-32 
+                            text-center transition-all active:scale-95">
+                    Login
+                </a>
+            </li>
+        @endif
+    </ul>
+
 </nav>
