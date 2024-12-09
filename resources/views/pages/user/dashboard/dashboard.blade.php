140133dd377fec9e526de6ddf4fb6b6da87163ac
Afonso Pereira <afonsogpereira2003@gmail.com>
2024-12-09 14:07:49 +0000

    profile stats styled

--- resources/views/pages/user/dashboard/dashboard.blade.php
+++ resources/views/pages/user/dashboard/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('inner_content')
     <!-- Main Content -->
     <div class="flex-1 bg-white">
-        <h3 class="text-2xl font-semibold text-[rgb(19,93,59)]">Welcome to Your Dashboard</h3>
+        <h3 class="text-2xl font-semibold text-gray-800">Welcome to Your Dashboard</h3>
         <p class="mt-2 text-gray-600">Here you can manage everything related to your account.</p>
         @include('components.user.userDescription')
     </div>
