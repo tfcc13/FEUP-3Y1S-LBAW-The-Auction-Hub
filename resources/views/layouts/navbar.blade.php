d67b7e37ca16511e3f4519225aa7b9bc83e22289
Afonso Pereira <afonsogpereira2003@gmail.com>
2024-12-13 11:49:42 +0000

    added create auction to nav bar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -42,7 +42,7 @@
                 </li>
             @endif
             <li class="flex items-center justify-center list-none">
-                <x-popup.popup position="bottom-left" width="w-44">
+                <x-popup.popup position="bottom-left" width="w-52">
                     <x-slot:trigger>
                         <button onclick="togglePopup(this)"
                             class="w-10 h-10 rounded-full overflow-hidden transition-all duration-150 active:scale-95 
@@ -60,8 +60,8 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                         stroke-linejoin="round">
-                                        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                        <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                                        <line x1="5" y1="12" x2="19" y2="12"></line>
                                     </svg>
                                     <span>Create Auction</span>
                                 </a>
