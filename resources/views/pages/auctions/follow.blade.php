3a3ac976363120af867adbf46fb6b3606f58c20e
Afonso Pereira <afonsogpereira2003@gmail.com>
2024-12-14 00:37:10 +0000

    updated following page

--- resources/views/pages/auctions/follow.blade.php
+++ resources/views/pages/auctions/follow.blade.php
@@ -1,18 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto px-4 py-8">
-        <h1 class="text-2xl font-bold mb-6">Followed Auctions</h1>
-        
+    <div class="flex flex-col items-center justify-center py-6 px-2 space-y-6">
+        <h1 class="text-2xl sm:text-4xl sm:font-semibold text-gray-800 text-center whitespace-nowrap">Followed Auctions</h1>
+
         @if ($followed->isNotEmpty())
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                 @foreach ($followed as $auction)
-                    <x-slide.slide-item 
-                        :title="$auction->title" 
-                        :currentBid="$auction->current_bid ?? $auction->start_price" 
-                        :imageUrl="asset($auction->primaryImage())" 
-                        :buttonAction="route('auctions.show', $auction->id)" 
-                    />
+                    <x-slide.slide-item :title="$auction->title" :currentBid="$auction->current_bid ?? $auction->start_price" :imageUrl="asset($auction->primaryImage())" :buttonAction="route('auctions.show', $auction->id)" />
                 @endforeach
             </div>
         @else
