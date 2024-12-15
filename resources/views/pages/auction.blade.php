0ad3fd3ef5842b8b78262f34c203e21824b00de2
avareno <miguelcabral798@gmail.com>
2024-12-15 15:31:32 +0000

    changes error message auction page

--- resources/views/pages/auction.blade.php
+++ resources/views/pages/auction.blade.php
@@ -14,12 +14,6 @@
             @endforeach
         </div>
     @endif
-    @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-
     @if (session('error'))
         <div class="alert alert-danger">
             {{ session('error') }}
