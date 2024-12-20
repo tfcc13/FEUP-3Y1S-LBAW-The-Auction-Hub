7958d3e9bc6d252e1f639535cae059c1d5e4cfff
avareno <miguelcabral798@gmail.com>
2024-12-20 01:37:54 +0000

    catefories in mobile have link

--- resources/views/components/categories/mobile-categories.blade.php
+++ resources/views/components/categories/mobile-categories.blade.php
@@ -1,19 +1,21 @@
 @props(['mappedCategories', 'iconMap'])
 
 <x-popup.popup position="bottom-left" width="w-64">
-    <x-slot:trigger>
-        <x-categories.category-item id="mobile-categories" text="Categories" icon="menu" onClick="togglePopup(this)" />
-    </x-slot:trigger>
+  <x-slot:trigger>
+    <x-categories.category-item id="mobile-categories" text="Categories" icon="menu" onClick="togglePopup(this)" />
+  </x-slot:trigger>
 
-    <x-slot:content>
-        <div class="space-y-4" role="list">
-            @foreach ($mappedCategories as $category)
-                <div class="flex items-center gap-3 p-2 hover:bg-gray-100 rounded-md cursor-pointer" role="listitem"
-                    tabindex="0">
-                    <span class="material-symbols-outlined" aria-hidden="true">{{ $iconMap[$category->name] }}</span>
-                    <span>{{ $category->name }}</span>
-                </div>
-            @endforeach
+  <x-slot:content>
+    <div class="space-y-4" role="list">
+      @foreach ($mappedCategories as $category)
+      <a href="{{ route('category.show', ['id' => $category->id]) }}">
+        <div class="flex items-center gap-3 p-2 hover:bg-gray-100 rounded-md cursor-pointer" role="listitem"
+          tabindex="0">
+          <span class="material-symbols-outlined" aria-hidden="true">{{ $iconMap[$category->name] }}</span>
+          <span>{{ $category->name }}</span>
         </div>
-    </x-slot:content>
+      </a>
+      @endforeach
+    </div>
+  </x-slot:content>
 </x-popup.popup>
